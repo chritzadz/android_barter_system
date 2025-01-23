f6298a4ca61afaa0480ca37b4a835c6ec8355994
Christian Anthony Dumanauw <ma283606@dal.ca>
2025-01-22 21:08:24 -0400

    UI design option

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import android.content.Intent
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +13,7 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge() //for UI
 
         // redirect to login
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, SignupActivity::class.java)
         startActivity(intent)
         finish() //pop from activity stack.
     }
--- app/src/main/java/com/example/myapplication/screens/SignupScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/SignupScreen.kt
@@ -9,13 +9,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.shape.*
+
 
 @Composable
 fun LoginScreen() {
     var email by remember { mutableStateOf(TextFieldValue("")) }
+    var username by remember { mutableStateOf(TextFieldValue("")) }
     var password by remember { mutableStateOf(TextFieldValue("")) }
+    var confirm_password by remember { mutableStateOf(TextFieldValue("")) }
 
     Column(
         modifier = Modifier
@@ -24,23 +29,49 @@ fun LoginScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Login", fontSize = 24.sp)
+        Text("Create New Account", fontSize = 24.sp)
+        // email, username, password, make sure password (i guess no need to create diff interface)
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
+        //username
         TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+            value = username, //email or username
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            shape = RoundedCornerShape(15.dp),
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
+        //email
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //password
         TextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password") },
+            //label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //make sure password
+        TextField(
+            value = confirm_password,
+            onValueChange = { confirm_password= it },
+            //label = { Text("Password") },
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
         )
@@ -48,7 +79,7 @@ fun LoginScreen() {
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = { /*logic here later on*/ }) {
-            Text("Login")
+            Text("Sign Up")
         }
     }
 }
